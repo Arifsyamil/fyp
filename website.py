c9be860ce64c4ebec557ed111d131d22eb5ed5da
Arifsyamil <121653152+Arifsyamil@users.noreply.github.com>
2023-01-16 08:27:30 +0800

    Update website.py

--- website.py
+++ website.py
@@ -12,6 +12,8 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 import numpy as np
 import matplotlib.pyplot as plt
 
+@st.cache
+
 #Page header, title
 st.set_page_config(page_title= "Malay Named Entity Recognition (NER) Model", page_icon= ":book:", layout= "wide")
 st.title(":book: Malay Named Entity Recognition (NER) model")
